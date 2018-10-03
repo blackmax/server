56c48caab40cd5b4916c84f567b171ca7ec96b32
MAXIM CHERNY <bm.black.max@gmail.com>
2018-10-03 22:45:14 +0300

    BEEP 2.0

--- beep.js
+++ beep.js
@@ -2,10 +2,24 @@ var io = require('socket.io')({
 	transports: ['websocket'],
 });
 
-io.attach(4567);
+io.attach(7777);
+
 
 io.on('connection', function(socket){
+
 	socket.on('beep', function(){
-		socket.emit('boop');
+		//ДЛЯ ВСЕХ
+        io.sockets.emit('boop', {some: socket.id});
+
+        // ДЛЯ ОДНОГО
+		socket.emit('boop', { some: socket.rooms });
 	});
-})
+	
+	socket.on('getUser', function (data) {
+		console.log(data);
+		socket.emit('userData', {some: data});
+    });
+
+
+});
+
