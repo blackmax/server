6fbbd2551241722f98d2ea46f21bd9b40adee0e4
MAXIM CHERNY <bm.black.max@gmail.com>
2018-10-11 00:02:53 +0300

    Пофикшенные юзвери

--- services/register_service.js
+++ services/register_service.js
@@ -20,25 +20,46 @@ class RegisterService extends Service {
 
             const userCar = this.ctx.db.user_cars.create({
                 user_id: user.id,
+                car_id: 1
+            });
+
+            const carSkin = this.ctx.db.user_skin.create({
+                user_id: user.id,
                 car_id: 1,
+                skin_id: 1,
+                new: 0
+            });
+
+            const carDisk = this.ctx.db.user_skin.create({
+                user_id: user.id,
+                car_id: 1,
+                skin_id: 129,
+                new: 0
             });
 
             const userIcon = this.ctx.db.user_icons.create({
                 user_id: user.id,
                 icon_id: 1,
+                new: 0
             });
 
             const endlessLevel = this.ctx.db.endless_levels.create({
-                user_id: user.id,
+                user_id: user.id
             });
 
-            const common = await Promise.all([userCar, userIcon, endlessLevel]);
+            const containers = this.ctx.db.containers.create({
+                user_id: user.id
+            });
+
+            const common = await Promise.all([userCar, carSkin, carDisk, userIcon, endlessLevel, containers]);
 
             const payload = {
                 user,
                 user_cars: [common[0]],
-                user_icons: [common[1]],
-                endless_level: common[2],
+                user_skin: [common[1], common[2]],
+                user_icons: [common[3]],
+                endless_level: common[4],
+                containers: common[5]
             };
 
 
